bc64c12ecafe5a1cff1d6297fe92309776521a73
Eugen Hotaj <ehotaj@google.com>
2017-09-26 23:51:44 -0400

    give explanation

--- neuro/hw2/pdf_eval.py
+++ neuro/hw2/pdf_eval.py
@@ -1,16 +1,19 @@
 import math
 
+THRESHOLDS = [2.69, 5.978, 5.667, 5.830]
+
 def build_gaussian_pdf(mu, sig):
   """Returns a Gaussian PDF for the given mean (mu) and standard deviation (sig)."""
   var = sig**2
   coef = 1/math.sqrt(2*math.pi*var)
   return lambda x: coef*math.exp(-(x - mu)**2/(2*var))
 
-g1 = build_gaussian_pdf(5, .5)
-g2 = build_gaussian_pdf(7, 1)
+# "Evaluate" which triggering threshold should be s.t. the penalaty for misidentifying
+# s2 is twice that of s1. (Answer: 5.978).
+s1 = build_gaussian_pdf(5, .5)
+s2 = build_gaussian_pdf(7, 1)
 
-xs = [2.69, 5.978, 5.667, 5.830]
-for x in xs:
-  print('x={}: {}'.format(x, g1(x)/g2(x)))
+for x in THRESHOLDS:
+  print('x={}: {}'.format(x, s1(x)/s2(x)))
 
  
