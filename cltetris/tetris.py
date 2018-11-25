37f2c5a3e015f04f80796cb2c3529b506871e862
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2018-11-24 21:25:18 -0500

    Add missing tetromino and gameover state.

--- cltetris/tetris.py
+++ cltetris/tetris.py
@@ -13,7 +13,8 @@ tetrominoes = {
     1: ['xx\nxx'],
     2: ['x\nx\nxx', 'xxx\nx', 'xx\n x\n x', '  x\nxxx'],
     3: ['x\nxx\n x', ' xx\nxx '],
-    4: ['xxx\n x ', ' x\nxx\n x', ' x \nxxx', 'x \nxx\nx '],
+    4: [' x\nxx\nx ', 'xx\n xx'],
+    5: ['xxx\n x ', ' x\nxx\n x', ' x \nxxx', 'x \nxx\nx '],
 }
 
 def process_input(key):
@@ -81,7 +82,7 @@ def reposition(game_board, tetromino, idx, x, dx, y, dy):
 def generate_tetromino():
     """Samples a new tetromino uniformly randomly."""
     tetromino = tetrominoes[np.random.randint(0, 5)]
-    idx = 0
+    idx = np.random.randint(0, len(tetromino))
     width = get_width(tetromino, idx)
     x = (WINDOW_WIDTH - width) // 2
     y = 0
@@ -109,7 +110,7 @@ def add_to_game_board(game_board, tetromino, idx, x, y):
 
 def clean_board(width, height):
     """Generates a new clean board with no tetrominoes."""
-    return [['.' for _ in range(width)] for _ in range(height)]
+    return [[' ' for _ in range(width)] for _ in range(height)]
 
 
 def clear_lines(game_board):
@@ -118,54 +119,70 @@ def clear_lines(game_board):
     height = len(game_board)
     new_board = clean_board(width, height)
     i = 0
+    lines_cleared = 0
     for line in reversed(game_board):
-        if '.' in ''.join(line):
+        if ' ' in ''.join(line):
             new_board[height - 1 - i] = line
             i += 1
-    return new_board
+        else: 
+            lines_cleared += 1
+    return new_board, lines_cleared
+
+
+def is_game_over(game_board):
+    x = WINDOW_WIDTH // 2
+    return (game_board[0][x-1] == 'x' or 
+            game_board[0][x] == 'x' or 
+            game_board[0][x+1] == 'x')
 
 
 def main():
     tcod.console_set_custom_font(
             'arial12x12.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
 
+    game_over = False
     speed = 1
     tetromino = None
     game_board = clean_board(WINDOW_WIDTH, WINDOW_HEIGHT)
-    last_time = time.time()
+    last_time = time.time() * 1000
     with tcod.console_init_root(WINDOW_WIDTH, WINDOW_HEIGHT, title='Tetris') as root:
         while not tcod.console_is_window_closed():
             key = tcod.console_check_for_keypress(flags=tcod.KEY_PRESSED)
             if key.vk == tcod.KEY_ESCAPE:
                 break
+            
+            if not game_over:
+                if not tetromino:
+                    tetromino, idx, x, y = generate_tetromino()
+                        
+                # Hanlde input.
+                dx, dy, didx  = process_input(key.vk) 
+                idx = (idx + didx) % len(tetromino)
+                x, y = reposition(game_board, tetromino, idx, x, dx, y, dy)
 
-            if not tetromino:
-                tetromino, idx, x, y = generate_tetromino()
-                    
-            # Hanlde input.
-            dx, dy, didx  = process_input(key.vk) 
-            idx = (idx + didx) % len(tetromino)
-            x, y = reposition(game_board, tetromino, idx, x, dx, y, dy)
-
-            # After some time, drop down and clear any filled lines.
-            curr_time = time.time()
-            if curr_time - last_time > speed:
-                _, y_new = reposition(game_board, tetromino, idx, x, 0, y, 1)
-                # If the tetromino can't move down any further it must be 
-                # frozen.
-                if y_new == y:
-                    game_board = add_to_game_board(game_board, 
-                                                   tetromino, idx, x, y)
-                    game_board = clear_lines(game_board)
-                    tetromino = None
-                else:
-                    y = y_new
-                last_time = curr_time
+                # After some time, drop down and clear any filled lines.
+                curr_time = time.time() * 1000
+                if curr_time - last_time > 1000 / speed:
+                    _, y_new = reposition(game_board, tetromino, idx, x, 0, y, 1)
+                    # If the tetromino can't move down any further it must be 
+                    # frozen.
+                    if y_new == y:
+                        game_board = add_to_game_board(game_board, 
+                                                       tetromino, idx, x, y)
+                        game_board, lines_cleared  = clear_lines(game_board)
+                        speed += lines_cleared
+                        tetromino = None
+                        game_over = is_game_over(game_board)
+                    else:
+                        y = y_new
+                    last_time = curr_time
 
             # Draw. 
             root.clear()
             draw_board = add_to_game_board(game_board, tetromino, idx, x, y)
             root.print_(0, 0, '\n'.join([''.join(row) for row in draw_board]))
+            if game_over:
+                root.print_(1, WINDOW_HEIGHT // 2, 'GAMEOVER')
             tcod.console_flush()
 
 if __name__ == '__main__':
